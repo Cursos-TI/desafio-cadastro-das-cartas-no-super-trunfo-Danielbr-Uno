01d0e0fcc1b32f18e3fe52858d9efec02f8d8905
Daniel Bruno <daniel.bruno.dev19@gmail.com>
2025-04-16 20:30:36 +0000

    Edição de código para nivel aventureiro(desafio 2)

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,9 +9,10 @@ int main() {
     double pibPerCap, pibPerCap2;
     long double superPoder, superPoder2;
     //Abaixo foram criadas variaveis para que possa ser dado o resultado das comparações entre as duas cartas.
-    int rPop, rArea, rPIB, rTur, rDP, rPPC, rSP;
-        
-    //Primeiro pego as informações da primeira carta.
+    //int rPop, rArea, rPIB, rTur, rDP, rPPC, rSP;
+
+    //Pegando as informações da primeira carta.
+/*  
     printf("Desafio cadastro de cartas Super Trunfo!\n\n");
     printf("Dados para primeira carta:\n");
     printf("Digite a letra que representa o Estado da primeira carta: \n");
@@ -34,18 +35,20 @@ int main() {
 
     printf("Digite o PIB da cidade: \n");
     scanf("%f", &pib);
-    
+*/
+
     //Abaixo o cálculo da densidade populacional e PIB per capta
     densiPop = populacao / area;
-    inverDensiPop = 1 / densiPop;
+    inverDensiPop = 1 / densiPop; // Invertendo o valor da densidade para calcular o super poder.
 
     pibB = pib * 1000000000; // Convertendo PIB para bilhões.
     pibPerCap = (double) pibB / populacao;
 
     //Abaixo o cálculo do super poder da primeira carta.
     superPoder = (long double) populacao + area + pib + pibPerCap + inverDensiPop + turismo;
-
-    //Agora pego as informações da segunda carta.
+    
+    //Pegando as informações da segunda carta.
+/*
     printf("\nDados para segunda carta:\n");
     printf("Digite a letra que representa o Estado da segunda carta: \n");
     scanf(" %c", &estado2);
@@ -67,10 +70,11 @@ int main() {
 
     printf("Digite o PIB da cidade: \n");
     scanf("%f", &pib2);
+*/
 
     //Abaixo o cálculo da densidade populacional e PIB per capta da segunda carta.
     densiPop2 = populacao2/area2;
-    inverDensiPop2 = 1 / densiPop2;
+    inverDensiPop2 = 1 / densiPop2; // Invertendo o valor da densidade para calcular o super poder.
 
     pibB2 = pib2 * 1000000000; //Convertendo PIB para bilhões
     pibPerCap2 = (double) pibB2 / populacao2;
@@ -79,6 +83,7 @@ int main() {
     superPoder2 = (long double) populacao2 + area2 + pib2 + pibPerCap2 + inverDensiPop2 + turismo2;
 
     //Abaixo o cálculo das comparações entre as duas cartas.
+/*
     rPop = populacao > populacao2;
     rArea = area > area2;
     rPIB = pib > pib2;
@@ -86,7 +91,8 @@ int main() {
     rDP = densiPop < densiPop2;
     rPPC = pibPerCap > pibPerCap2;
     rSP = superPoder > superPoder2;
-
+*/
+    
     printf("\nCarta número 1: \n");
     printf("Código: %s \n", codigo);
     printf("Estado: %c \n", estado);
@@ -111,6 +117,8 @@ int main() {
     printf("PIB per capta: %.2f reais por habitante \n", pibPerCap2);
     printf("Super poder: %.2Lf \n\n", superPoder2);
 
+    // Duelo de cartas utilizando estruturas de decisão encadeadas.
+/*
     printf("\nDuelo de cartas: \n");
     if(populacao > populacao2){
         printf("Carta 1(%s) venceu com %u habitantes!\n", cidade, populacao);
@@ -147,6 +155,9 @@ int main() {
     } else {
         printf("Carta 2(%s) venceu com %.2Lf pontos!\n", cidade2, superPoder2);
     }
-
+*/
+    
+    // Duelo de cartas utilizando estrutura switch e estruturas encadeadas, que possibilitam o usuário interagir com mais facilidade.
+    
     return 0;
 }
